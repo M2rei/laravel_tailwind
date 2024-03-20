c4abc798d2a2f24c5dd04eb4d59f5503acf1693c
Muflih Raihan <muflihraihan693@gmail.com>
2024-03-20 13:14:49 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="flex justify-start ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+    </div>
+</body>
+
+</html>
